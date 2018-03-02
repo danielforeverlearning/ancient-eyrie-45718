40da2a0f270bb305a3a122034050a3e5312a625d
Max Lee <maxlee@pacrim.edu>
2018-03-02 02:18:24 -0800

    asdfasfd

--- index.js
+++ index.js
@@ -105,7 +105,6 @@ function MTGO_performRequest(endpoint, method, data, success) {
     });
 
     res.on('end', function() {
-      req.write(responseString);
       console.log("oh wow got to end event yay");
       console.log(responseString);
       var responseObject = JSON.parse(responseString);
