8452b281126d320adfe58d19f9b9e10ae6359697
Max Lee <maxlee@pacrim.edu>
2018-03-06 09:53:27 -0800

    forgot to add end

--- index.js
+++ index.js
@@ -151,6 +151,7 @@ function endbattleaxe() {
     console.log("inside endbattleaxe");
     console.log(savestr);
     saveres.write("<p>" + savestr + "</p>");
+    saveres.end();
 }
 
 function battleaxe(res) {
